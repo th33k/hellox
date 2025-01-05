1e6d4804fac235b59a2c6447b2031ce9893fcb41
Theekshana udara <74367192+th33k@users.noreply.github.com>
2025-01-05 17:41:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react';
 // import './App.css'
 import HomePage from './pages/HomePage'  // Import HomePage component
 
